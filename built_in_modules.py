c3fa2d8cccafee0786ac3fc178b7724a5a2f9a86
Anna-K-S <anna.sergeevna1622@gmail.com>
2022-12-06 02:16:45 +0300

    Some built in modules in Python

---
+++ built_in_modules.py
@@ -0,0 +1,32 @@
+# Модуль - файл с python кодом
+# import random
+# x = random.randint(1, 10)
+# print(x)
+
+# from random import randint
+
+# импорт функции из модуля 'random"
+# a = randint(1, 10)
+# print(a)
+
+# from random import shuffle as shuffle_my_list
+# импорт функции под заданным именем
+# <название функции> as <новое имя функции>
+
+# my_list = [1, 2, 3, 4, 5]
+# shuffle_my_list(my_list)
+# print(my_list)
+
+import math
+
+b = math.sqrt(68)
+# 'sqrt()' возвращает квадратный корень числа
+print(b)
+
+x = math.ceil(1.4)
+# '.ceil()' - округляет число в большую сторону
+y = math.floor(1.4)
+# '.floor()' - округляет число в меньшую сторону
+
+print(x)  # 2
+print(y)  # 1
