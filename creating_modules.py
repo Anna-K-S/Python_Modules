22a62cc02743befeadfc0ede7cfb35ccba141693
Anna-K-S <anna.sergeevna1622@gmail.com>
2022-12-06 02:45:15 +0300

    Create modules

---
+++ creating_modules.py
@@ -0,0 +1,12 @@
+# import greetings
+#
+# greetings.say_hi()
+#
+# from greetings import say_hello
+# say_hello()
+
+# import goodbyes
+# goodbyes.say_bye_bye()
+
+# from goodbyes import say_good_bye
+# say_good_bye()
